d2b9779ea9c1f24569b99158535b4474906fdee0
gorik0 <delavarus@gmail.com>
2024-11-25 13:21:50 +0300

    setup_dirs_structure

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"os"
+	"strings"
+)
+
 func main() {
 
 	println("HeloSo")
+	lisDir := strings.Split("auth comment restaurants food user session", " ")
+	for _, r := range lisDir {
+		os.MkdirAll("proto/"+r, 0777)
 
+	}
 }
